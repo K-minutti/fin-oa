cd661664b964de87ddcda287c0b052ac633a6a0d
Kevin Minutti <minutti70@gmail.com>
2021-03-26 18:33:26 -0400

    front-end setup

---
+++ client/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+
+	return  (
+	<div id="container"> 
+	<h1>MAIN - MANE</h1>
+		<p>Hello this is the main where everything will render</p>
+	</div>
+	)
+}
