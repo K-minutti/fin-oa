d228ae74e909686e4d2077f6b9cc25b7e0a7a1ec
Kevin Minutti <minutti70@gmail.com>
2021-03-26 18:49:57 -0400

    creating sidebar

--- client/Main.js
+++ client/Main.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 
 const Main = () => {
 
 	return  (
-	<div id="container"> 
+	<div id="main" className="row container>
+	<Sidebar/>
+	<div class="container">
+
+	
 	<h1>MAIN - MANE</h1>
 		<p>Hello this is the main where everything will render</p>
 	</div>
+	</div>
 	)
 }
+
+export default Main
---
+++ client/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+	return (
+		<div id="sidebar">
+
+		<section>
+		<h4>HELLOOOO</h4>
+		</section>
+	
+		</div>
+	)
+}
+
+export default Sidebar
