dc58c40af71c01f26d94c35ba645ad56963f6aaa
Kevin Minutti <minutti70@gmail.com>
2021-03-27 17:51:59 -0400

    UI styling

--- client/Header.js
+++ client/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
-
 const Header = () => {
   return (
     <nav className="row">
-      <h1>Open Analytics</h1>
+      <h1 style={{ fontSize: "30px", letterSpacing: "0.08em" }}>
+        <span className="fas fa-circle-notch x-large"></span>Open Analytics
+      </h1>
       <div className="navbar">
-        <a className="nav-item">BELL</a>
-        <a className="nav-item">PROFILE ICON</a>
+        <a className="nav-item">
+          <span className="far fa-bell large"></span>
+        </a>
+        <a className="nav-item">
+          <span className="far fa-user-circle large"></span>
+        </a>
       </div>
     </nav>
   );
--- client/Sidebar.js
+++ client/Sidebar.js
@@ -5,20 +5,36 @@ const Sidebar = () => {
   return (
     <div id="sidebar">
       <section>
+        <div style={{ width: "85%", borderBottom: "1px solid #cecece" }}>
+          <span
+            className="fas fa-circle-notch large"
+            style={{ color: "white", marginBottom: "36px" }}
+          ></span>
+        </div>
         <Link to="/overview">
-          <h3 className="sidebar-item">Overview</h3>
+          <h3 className="sidebar-item">
+            <span className="fas fa-table"></span>Overview
+          </h3>
         </Link>
         <Link to="/biasDetection">
-          <h3 className="sidebar-item">Bias Detection</h3>
+          <h3 className="sidebar-item">
+            <span className="fas fa-bullseye"></span>Bias Detection
+          </h3>
         </Link>
         <Link to="/officer360">
-          <h3 className="sidebar-item">Officer 360</h3>
+          <h3 className="sidebar-item">
+            <span className="fas fa-street-view"></span>Officer 360
+          </h3>
         </Link>
         <Link to="/data">
-          <h3 className="sidebar-item">Data</h3>
+          <h3 className="sidebar-item">
+            <span className="fas fa-file-upload"></span>Data
+          </h3>
         </Link>
         <Link to="/settings">
-          <h3 className="sidebar-item">Settings</h3>
+          <h3 className="sidebar-item">
+            <span className="fas fa-sliders-h"></span>Settings
+          </h3>
         </Link>
       </section>
     </div>
